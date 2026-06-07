698fe8190fe84ba9587672f7a8b595b8508324be
caiwen <2970046657@qq.com>
2026-06-07 14:01:56 +0800

    feat: 休眠机制

---
+++ kernel/src/process/timer.rs
@@ -0,0 +1,115 @@
+use core::{cmp::Ordering, time::Duration};
+
+use crate::{
+    exception::timer::jiffies,
+    process::{
+        cpu::CPUManager,
+        schedule::TaskScheduler,
+        task::{TaskControlBlock, TaskStatus},
+    },
+    sync::spin::SpinMutex,
+};
+use alloc::{collections::binary_heap::BinaryHeap, sync::Arc};
+use lazy_static::lazy_static;
+
+#[inline]
+pub fn time_us() -> usize {
+    jiffies() * 1000
+}
+
+struct SleepTimer {
+    expire_us: usize,
+    task: Arc<TaskControlBlock>,
+}
+
+impl PartialEq for SleepTimer {
+    fn eq(&self, other: &Self) -> bool {
+        self.expire_us == other.expire_us
+    }
+}
+impl Eq for SleepTimer {}
+impl PartialOrd for SleepTimer {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for SleepTimer {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.expire_us.cmp(&other.expire_us)
+    }
+}
+
+lazy_static! {
+    static ref SLEEP_TIMER_QUEUE: SpinMutex<BinaryHeap<SleepTimer>> =
+        SpinMutex::new(BinaryHeap::new(), "sleep_timer");
+}
+
+/// 休眠当前线程，直到 `expire_us` 时间后唤醒
+///
+/// # Parameters
+///
+/// - `expire_us`: 到期时间，单位为微秒
+///
+/// # Preconditions
+///
+/// 调用本函数时，不能持有自旋锁
+pub fn sleep_with_expire(expire_us: usize) {
+    // 没用 condvar 来做睡眠和唤醒
+    // 用 condvar 的话需要取 condvar 加入堆之后的引用来睡眠
+    // 而 BinaryHeap 不支持插入元素后返回元素在堆中引用
+    let current_task = CPUManager::current_task().unwrap();
+    let sleep_timer = SleepTimer {
+        expire_us,
+        task: current_task.clone(),
+    };
+    let mut queue = SLEEP_TIMER_QUEUE.lock();
+    queue.push(sleep_timer);
+    let mut task_inner = current_task.lock();
+    // 拿到 task_inner 之后再释放 queue，防止我们还没完成睡眠就被唤醒了
+    drop(queue);
+    task_inner.status = TaskStatus::Blocked;
+    let current_context = &mut task_inner.context as *mut _;
+    // 有可能回到调度循环之后，当前任务被杀死，然后永远回不来了
+    // 所以这里需要搞一个类似 [CPU::yield_current_task] 的操作
+    unsafe { task_inner.leak() };
+    drop(task_inner);
+    drop(current_task);
+    let cpu = unsafe { CPUManager::current_cpu() };
+    cpu.go_scheduler(current_context);
+
+    let cpu = unsafe { CPUManager::current_cpu() };
+    let current_task = cpu.current_task.clone().unwrap();
+    // SAFETY: 到这里说明从调度循环回来了。在回来之前，调度循环会加锁
+    unsafe { current_task.unlock() };
+}
+
+/// 休眠当前线程，直到过了 `interval_us` 之后唤醒
+///
+/// # Parameters
+///
+/// - `interval_us`: 间隔时间，单位为微秒
+///
+/// # Preconditions
+///
+/// 调用本函数时，不能持有自旋锁
+#[inline]
+pub fn sleep_with_interval(interval: Duration) {
+    let current_time = time_us();
+    sleep_with_expire(current_time + interval.as_micros() as usize);
+}
+
+pub fn check_sleep_timer() {
+    let mut queue = SLEEP_TIMER_QUEUE.lock();
+    let current_time = time_us();
+    while let Some(sleep_timer) = queue.peek() {
+        if sleep_timer.expire_us <= current_time {
+            let mut task_inner = sleep_timer.task.lock();
+            task_inner.status = TaskStatus::Ready;
+            TaskScheduler::push(sleep_timer.task.clone());
+            drop(task_inner);
+            queue.pop();
+        } else {
+            break;
+        }
+    }
+}
